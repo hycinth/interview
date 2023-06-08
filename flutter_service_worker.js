41f8e3e28bcdf056f69868efefdcc786ceb941bb
wuhaiqiang <wuhaiqiang@bme.com>
2023-06-08 16:14:04 +0800

    bug fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "ec22bbcfc3bec4656059be6fe480e10e",
-"index.html": "9060b82c3c7418ee63fc07f00951357c",
-"/": "9060b82c3c7418ee63fc07f00951357c",
-"main.dart.js": "f1d2f14a8d7825d705663fc5a13fd21f",
+"index.html": "805f7684651cf63c45ad495fa61bdc4a",
+"/": "805f7684651cf63c45ad495fa61bdc4a",
+"main.dart.js": "d2d61e76f65984c4f51c98369e68476f",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
